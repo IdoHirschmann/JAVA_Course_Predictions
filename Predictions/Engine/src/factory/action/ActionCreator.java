3b15fb161fcf23f48227c99a7e3bce7d450448b8
RonYakov <r.yakov154@gmail.com>
2023-08-11 18:05:03 +0300

    Create ActionCreator.java

---
+++ Predictions/Engine/src/factory/action/ActionCreator.java
@@ -0,0 +1,11 @@
+package factory.action;
+
+import rule.action.api.Action;
+import schema.generated.PRDAction;
+
+public class ActionCreator {
+
+    public static Action createAction(PRDAction prdAction) {
+        return null;
+    }
+}
